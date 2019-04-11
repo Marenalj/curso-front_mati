54e3c5bc9e62d2e8f6984c69cf8035204a637ea5
marenalj <jorquera2009@hotmail.com>
2019-04-11 13:42:41 +0200

    cajas

---
+++ 11_var/js/app.js
@@ -0,0 +1,8 @@
+/* js es un lenguaje de programación y hay que tener en cuenta unas variables. La utilizamos para almacenar la información. Una variable es un nombre al que le asigno un valor. Las variables le asigno let (voy a crear..)más una variable. Los espacios no cuentan. Es para guardar un número en una variable. Estoy guardando número asociado a números para que me haga una operación. console.log(suma) y console.dir(suma); va a salir lo mismo,  para que salga por consola. 
+En el servidor de Visual me voy a la carpeta js: cómo?? Node-cd .\11_var\   -   cd js   -*/
+let x = 12
+let y = 8
+let suma 
+suma = x + y 
+console.log(suma)
+/*  console.dir(suma) daría el mismo resultado que con console.log(suma). escribes en el servidor     node app.js     y te da el resultado*/ 

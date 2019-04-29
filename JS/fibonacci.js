c85d48ffa66e323c32ae37c1dc44391af83612d2
marenalj <jorquera2009@hotmail.com>
2019-04-29 11:28:53 +0200

    fibonacci

--- JS/fibonacci.js
+++ JS/fibonacci.js
@@ -1,29 +1,17 @@
    /**
- *    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
- *    fnum  = f(n - 1) + f(n - 2)
- *    cuando n >= 2                    (que sería igual que n > 1)
+ *         
  * 
- * function saludar(nombre) {
-  alert('Hola ' + nombre);
-}
-
-function procesarEntradaUsuario(callback) {
-  var nombre = prompt('Por favor ingresa tu nombre.');
-  callback(nombre);
-}
-
-procesarEntradaUsuario(saludar);
- * 
- * 
- * @param: limit
- * @returns: fibonacciArray
- * 
- */
-
-
+ * El del profe:
+ 
  /**
+  * 
+  *   El mío:
+  *   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
+  *   fnum  = f(n - 1) + f(n - 2)
+  *   cuando n >= 2                (que sería igual que n > 1) 
+  * 
   * La primera iteración siempre te va a devolver un 0 y un 1
-  * La segunda iteración va a entrar dentro del while porque el límite no es 1 y el Array va a ser igual al Fibonacci de la iteración 1. 
+  * La segunda iteración va a entrar dentro del while porque el límite no es 1 y el Array va a ser igual al Fibonacci de la iteración 1
   *  let fibonacciArray = fibonacci(1);
   * Cada iteración suma los dos anteriores
   * 

b17cd71a98d00047764a2cd2545638cc8337c170
Marenalj <jorquera2009@hotmail.com>
2019-05-06 22:09:15 +0200

    cadena

---
+++ JS/romper_cadena.js
@@ -0,0 +1,11 @@
+
+function romperCadena(cadenaLetras) {
+    
+    var cadenaLetras;
+    cadenaLetras = 'abcdefghijklmnopqrstuvwxyz';
+    cadena.substr(inicio[13])
+
+
+
+    console.log("(26):"  + cadenaLetras.substr(13));   
+}

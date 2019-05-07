a97e71a2e085f79fa2d05cbe1d6ddf91e1a18e06
marenalj <jorquera2009@hotmail.com>
2019-05-07 11:02:49 +0200

    arrays

--- JS/29_arrays.js
+++ JS/29_arrays.js
@@ -1,3 +1,29 @@
 let datos =  []
 // let datos = new Array()   --> Array es la función constructora
+
 datos = [1,2,3,5,7,9]
+datos.push(45)
+
+function numeroAlAzar100() {
+    return parseInt(Math.random()*100)
+}
+
+/**
+ * Crea un array de n aleatorios, como mínimo 1
+ * 
+ * function aleatorios
+ * @param: limite: number
+ * @returns: array[number] --->una lista
+ * 
+ */
+// inicializo r con un array vacío
+
+ function aleatorios(limite = 1) {
+     let r = []                    
+     for (let i = 0; i < limite; i++) {
+         r[i] = numeroAlAzar100()
+         
+     }
+     return r
+ }
+ console.log(aleatorios(12))

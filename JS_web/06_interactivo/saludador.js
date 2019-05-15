29d9c8eeb771171cd4db6f1628228c0eb4e0733f
marenalj <jorquera2009@hotmail.com>
2019-05-15 11:30:06 +0200

    input text sin clases

--- JS_web/06_interactivo/saludador.js
+++
@@ -1,8 +0,0 @@
-/**
- * Las clases siempre hacen referencia a sustantivos porque son pasivas, como personas, objetos... y a los métodos se les dará nombres de verbos porque son activos. Saludadro va a saludar. Saludar es el método. aQuien = ''    va a ser un string y se deja espacio vacío.
- */
-
- /** Seleccionar nodos del DOM */
- // Mis nodos del DOM van a ser quién (la entrada de datos y los botones)
-
-    

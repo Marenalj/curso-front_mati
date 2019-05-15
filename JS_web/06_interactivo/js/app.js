5d999987dce3044be8d54f03ad9b350a96b438ab
Marenalj <jorquera2009@hotmail.com>
2019-05-15 23:18:28 +0200

    añadir botón borrar en función main

--- JS_web/06_interactivo/js/app.js
+++ JS_web/06_interactivo/js/app.js
@@ -7,3 +7,4 @@
 import { main } from './saludador.js'
 
 document.addEventListener('DOMContentLoaded', main ) 
+
--- JS_web/06_interactivo/saludador.js
+++ JS_web/06_interactivo/saludador.js
@@ -6,3 +6,45 @@
  // Mis nodos del DOM van a ser quién (la entrada de datos y los botones)
 
     
+export function main () {
+
+    let aQuien = ''
+    let msg = [`Hola `, `Adios `]
+
+    /** Seleccionar nodos del DOM */
+    let inNombre = document.querySelector('#in-nombre')
+    let btnSaludar = document.querySelector('#btn-saludar')
+    let btnDespedirse = document.querySelector('#btn-despedirse')
+    let btnBorrar = document.querySelector('#btn-borrar')
+    let output = document.querySelector('#output')
+    
+    /**Asignar manejadores a los nodos */
+    btnSaludar.addEventListener('click', saludar)
+    /* btnSaludar.addEventListener('click', () => {
+            aQuien = inNombre.value
+            console.log(msg[0] + aQuien)
+    }) */
+    btnDespedirse.addEventListener('click', despedirse)
+    btnBorrar.addEventListener('click', borrar)
+
+    function saludar () {
+        if (inNombre.value) {
+            aQuien = '<b>' + inNombre.value + '</b>'
+            // console.log(msg[0] + aQuien)
+            output.innerHTML = msg[0] + aQuien
+        }
+    }
+
+    function despedirse() {
+        if (inNombre.value) {
+            aQuien = '<b>' + inNombre.value + '</b>'
+            // console.log(msg[1] + aQuien)
+            output.innerHTML = msg[1] + aQuien 
+        }
+    }
+
+    function borrar() {
+        output.innerHTML = ''
+        inNombre.value = ''
+    }
+}

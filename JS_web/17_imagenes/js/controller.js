68d6913b59ef351dee23b6df1be6374adab1bf78
Marenalj <jorquera2009@hotmail.com>
2019-06-06 20:02:21 +0200

    imagenes

--- JS_web/17_imagenes/js/controller.js
+++ JS_web/17_imagenes/js/controller.js
@@ -1,36 +1,29 @@
 export function controller() {
     console.log('Controller cargado')
 
-//variable dragon propiedad true
-   // let dragon = true
+//variable stich propiedad true
+   // let stich = true
 
    // .img_dinamic
    // .button
 
-   export function controller() {
-    console.log('Controller cargado')
-
 
     function onClickImg(ev) {
         let nodoImg = ev.target.previousElementSibling.firstElementChild
-
-// ev.target es el botón en el que yo he hecho click pero si pongo previousElementSibling y accedo a firstElementChild, llego a la imagen
-        console.log(nodoImg.id)
-
         if (nodoImg.id == 'img1') {
             if (img1_stich) {
-                nodoImg.src = "./assets/stich_depie.png"
+                nodoImg.src = "./assets/stich_depie.jpg"
             } else {
-                nodoImg.src = "./assets/dragon_komodo.jpg"
+                nodoImg.src = "./assets/peluche-stitch-lilo-stich-disney-stitch-32-cm-D_NQ_NP_682403-MCO25758255829_072017-F.jpg"
             }
             img1_dragon = !img1_stich 
         } else if (nodoImg.id == 'img2') {
             if (img2_dragon) {
                 nodoImg.src = "./assets/peluche-stitch-lilo-stich-disney-stitch-32-cm-D_NQ_NP_682403-MCO25758255829_072017-F.jpg"
             } else {
-                nodoImg.src = "./assets/D_NQ_NP_932687-MLM28574486981_112018-O.jpg"
+                nodoImg.src = "./assets/stich_depie.jpg"
             }
-            img2_dragon = !img2_dragon
+            img2_stich = !img2_stich_sentado
         } 
     }
 }

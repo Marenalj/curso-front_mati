14ddf282b0e585298700a43384b990d722a9dd40
Marenalj <jorquera2009@hotmail.com>
2019-06-06 19:47:55 +0200

    imagenes

--- JS_web/17_imagenes/js/controller.js
+++ JS_web/17_imagenes/js/controller.js
@@ -1,41 +1,36 @@
 export function controller() {
     console.log('Controller cargado')
+
 //variable dragon propiedad true
    // let dragon = true
 
    // .img_dinamic
    // .button
 
-   let img1_dragon = true
-   let img2_dragon = false
-
-   aBotones = document.querySelectorAll('.button')
-   aBotones.forEach(item => {
-       item.addEventListener('click', onClickImg)
-   });
+   export function controller() {
+    console.log('Controller cargado')
 
 
     function onClickImg(ev) {
-        let nodoImg = ev.target.previousElementsSibling.firstElementChild
+        let nodoImg = ev.target.previousElementSibling.firstElementChild
+
 // ev.target es el botón en el que yo he hecho click pero si pongo previousElementSibling y accedo a firstElementChild, llego a la imagen
         console.log(nodoImg.id)
 
         if (nodoImg.id == 'img1') {
-            if (img1_dragon) {
-                nodoImg.src = "./assets/iguana.png"
+            if (img1_stich {
+                nodoImg.src = "./assets/stich_depie.png"
             } else {
-                nodoImg.src="./assets/dragon_komodo.jpg"
+                nodoImg.src = "./assets/dragon_komodo.jpg"
             }
-            img1_dragon = !img1_dragon
+            img1_dragon = !img1_stich 
         } else if (nodoImg.id == 'img2') {
             if (img2_dragon) {
-                nodoImg.src = "./assets/iguana.png"
+                nodoImg.src = "./assets/peluche-stitch-lilo-stich-disney-stitch-32-cm-D_NQ_NP_682403-MCO25758255829_072017-F.jpg"
             } else {
-                nodoImg.src="./assets/dragon_komodo.jpg"
+                nodoImg.src = "./assets/D_NQ_NP_932687-MLM28574486981_112018-O.jpg"
             }
             img2_dragon = !img2_dragon
-        }
-        }
-
-/*       */
+        } 
+    }
 }

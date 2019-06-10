9d379c2841d6659095705c562e3e6078cb14b267
marenalj <jorquera2009@hotmail.com>
2019-06-10 13:59:59 +0200

    form dinamic

---
+++ JS_web/18_form_dinamic/js/controller.js
@@ -0,0 +1,41 @@
+import { GENEROS } from "./datos.js";
+
+export function controller {
+    console.log('Controller cargado')
+    console.log(GENEROS)
+    const aGeneros = GENEROS
+    let html = ''
+
+    let selectGeneros = document.querySelector('#generos')
+    let selectAutores = document.querySelector('#autores')
+    let btnPedir = document.querySelector('#btnPedir')
+
+    selectGeneros.addEventListener('change', onChangeGenero)
+    selectAutores.addEventListener('change', onChangeAutores)
+
+    aGeneros.forEach ( item => {
+        console.log(item )
+        html += '<option value=""></option>' 
+    })
+
+    selectGeneros.innerHTML = html
+    
+   function onChangeGenero(ev) {
+       let i = ev.target.selectedIndex
+       let aAutores = aGeneros[i].autores
+       let html = '<option value=""></option>'
+
+       aAutores.forEach ( item => {
+           html += '<option value="${item.value}">${item.label}</option>' 
+        })
+    
+        selectAutores.innerHTML =  html
+        selectAutores.focus()
+   }
+
+   function onChangeAutores(ev) {
+       console.log(ev.target.selectedIndex)
+   }
+
+
+}

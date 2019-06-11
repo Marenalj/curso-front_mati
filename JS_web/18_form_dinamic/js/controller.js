750b8ec09132ce0d7e549c8510ceabe9d8fc2b97
marenalj <jorquera2009@hotmail.com>
2019-06-11 11:13:19 +0200

    form dinamic

--- JS_web/18_form_dinamic/js/controller.js
+++ JS_web/18_form_dinamic/js/controller.js
@@ -4,8 +4,9 @@ export function controller () {
     console.log('Controller cargado')
     console.log(GENEROS)
     const aGeneros = GENEROS
+    let iGenero
+    let iAutor
     let html = ''
-
 
     let selectGeneros = document.querySelector('#generos')
     let selectAutores = document.querySelector('#autores')
@@ -13,6 +14,9 @@ export function controller () {
 
     selectGeneros.addEventListener('change', onChangeGenero)
     selectAutores.addEventListener('change', onChangeAutores)
+
+    btnPedir.addEventListener('click', onClickPedir)
+    //btnPedir.onclick = onClickPedir                 forma antigua de definir los eventos
 
 
     aGeneros.forEach ( item => {
@@ -22,21 +26,83 @@ export function controller () {
     selectGeneros.innerHTML = html
 
     function onChangeGenero(ev) {
-        let i = ev.target.selectedIndex
-        let aAutores = aGeneros[i].autores
-        let html = '<option value=""></option>'
-        
-        aAutores.forEach ( item => { 
-            html += `<option value="${item.value}">${item.label}</option>`     
-        })
-
+        iGenero = ev.target.selectedIndex
+        let aAutores = []
+        html = '<option value=""></option>'
+        if (iGenero) {
+            aAutores = aGeneros[iGenero].autores
+            aAutores.forEach ( item => { 
+                html += `<option value="${item.value}">${item.label}</option>`     
+            })    
+            selectAutores.focus()
+        } 
         selectAutores.innerHTML = html
-        selectAutores.focus()
+        btnPedir.textContent = 'Pedir titulos'
+        btnPedir.disabled = true
+
     }
      
 
     function onChangeAutores (ev) { 
-        console.log(ev.target.selectedIndex) 
+        if (ev.target.selectedIndex) {
+            iAutor = ev.target.selectedIndex - 1
+            btnPedir.textContent = 'Pedir titulos de ' + aGeneros[iGenero].autores[iAutor].label
+            btnPedir.disabled = false
+        } else {
+            btnPedir.textContent = 'Pedir titulos'
+            btnPedir.disabled = true
+        }
+    }
+
+    function onClickPedir() {
+        console.clear()
+        console.log('Iniciando petición')
+        ajax(aGeneros[iGenero].autores[iAutor].value)
     }
     
+}
+
+function ajax(clave) {
+
+    const url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${clave}&fields=items(volumeInfo(publisher,title,language))`
+
+    const http = new XMLHttpRequest()
+
+    http.addEventListener('readystatechange', onResponse)
+    //http.onreadystatechange = onResponse
+
+    http.open('GET', url)
+    http.send(null)
+
+    function onResponse() {
+        console.log(http.readyState)
+        if (http.readyState == 4 && http.status == 200) {
+            procesarRespuesta(http.responseText)
+        }
+    }
+}
+
+function procesarRespuesta(response) {
+    let aDatos = JSON.parse(response).items
+    console.log(aDatos)
+    let aDatosFinal = aDatos.map(item => {item.volumenInfo} )
+    console.log(aDatosFinal)
+
+}
+
+function mostrarRespuesta(aDatos) {
+
+    crearTabla() {
+        let output = document.querySelector('#output')
+        let tabla = '<table>'
+        table += '<tr><th>Título</th><th>Editorial</th><th>Idioma</th></tr>'
+        aDatos.forEach( (item, i) => tabla += 
+        `<tr>
+        <td>${item}</td>
+        <td>${item.publisher}</td>
+        <td>${item.language}</td></tr>`)
+
+        tabla += '</table>'
+        this.tabla.innerHTML = tabla
+    }
 }

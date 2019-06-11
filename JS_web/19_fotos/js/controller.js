e3c4017b3af6fd727e23d01d21564714976f426b
Marenalj <jorquera2009@hotmail.com>
2019-06-11 18:33:09 +0200

    fotos desde JSON placeholder

--- JS_web/19_fotos/js/controller.js
+++ JS_web/19_fotos/js/controller.js
@@ -1,59 +1,57 @@
-import { ajax } from "./ajax.js"
+import { ajax } from "./ajax.js";
 
 export function controller() {
-    const URL = 'https://jsonplaceholder.typicode.com/photos'
+    const URL = 'https://jsonplaceholder.typicode.com/photos?albumId=1'
     const URL_USERS = "https://randomuser.me/api/?results=10"
     const aFotos = []
     let sectionFigures = document.querySelector('#figures')
-    ajax(URL, 'GET', getFotos)
+    ajax(URL, 'GET', getFotos )
 
+    // Elementos del DOM
+    const btnAdd = document.querySelector('#put')
+    let aBtnModificar 
+    let aBtnBorrar 
 
-// Elementos del DOM
+    // Manejadores de eventos
+    btnAdd.addEventListener('click', onAdd)
+    
 
-let btnAdd = document.querySelector('#put')
-let aBtnModificar
-let aBtnBorrar
-
-btnAdd.addEventListener('click', onAdd)
-abtnModificar.forEach(item => item.addEventListener('click', onAdd))
-abtnBorrar.forEach(item => item.addEventListener('click', onDelete))
-
-/** "albumId": 1,
- * "":
- */
-
+    /*  "albumId": 1,
+    */   
     function getFotos(response) {
         console.log(response)
         let html = ''
-
-        JASON.parse(response. forEach( item => {
+        JSON.parse(response)     
+            .forEach( item => {
                 html += `
                     <figure>
-                    <a href="${item.url}">
-                    <figcaption>${item, title}</figcaption>
-                    <img src="${item, thumbnailUrl}" alt="">
-                    </a>
-                    <button id="post" id="post"_${item.id}>Modificar</button>
-                    <button class="delete" id="delete_${item.id}">Borrar</button>
+                        <a href="${item.url}">
+                        <figcaption>${item.title}</figcaption>
+                        <img src="${item.thumbnailUrl}" alt="">
+                        </a>
+                        <button class="post" id="post_${item.id}">Modificar</button>
+                        <button class="delete" id="delete_${item.id}">Borrar</button>
                     </figure>
                 `
-            })
-        });
+            });
 
-        sectionFigures.InnerHTML = html
-        = document.querySelectorAll('.post')
-        = document.querySelectorAll('.delete')
-    }   
+        sectionFigures.innerHTML = html
+        aBtnModificar = document.querySelectorAll('.post')
+        aBtnBorrar= document.querySelectorAll('.delete')
+        aBtnModificar.forEach( item => item.addEventListener('click', onModify))
+        aBtnBorrar.forEach( item => item.addEventListener('click', onDelete))
+    }
+
 
     function onAdd () {
         console.log('Añadiendo')
     }
 
-    function onModify (ev) {
+    function onModify(ev) {
         console.log('Modificando', ev.target.id)
     }
 
-    function onDelete() {
+    function onDelete(ev) {
         console.log('Borrando', ev.target.id)
     }
 }

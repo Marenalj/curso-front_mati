f5860d33bab9dd0498650381f81e6035b7fe7cf3
marenalj <jorquera2009@hotmail.com>
2019-06-11 13:48:14 +0200

    form dinamic

---
+++ JS_web/18_form_dinamic/js/form_helpers.js
@@ -0,0 +1,18 @@
+
+
+function setRadio(radio) {
+// función some hace una función hasta que la función retorne un true, no da más vueltas y no lo repita más veces
+    radio.some()
+
+    let value
+    for (let i = 0; i < radiolength; i++) {
+        const item = radio[i];
+        it (item.checked) {
+            return item.value
+        }
+    }
+    return 
+            }
+        }
+    )
+}

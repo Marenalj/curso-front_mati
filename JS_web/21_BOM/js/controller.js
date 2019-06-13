509da101d5d9d6789ac3cc0253bf300625b1156c
Marenalj <jorquera2009@hotmail.com>
2019-06-13 23:03:59 +0200

    BOM

--- JS_web/21_BOM/js/controller.js
+++ JS_web/21_BOM/js/controller.js
@@ -1,45 +1,53 @@
 export function controller() {
-  console.log('Controller cargado')
+    console.log('Controller cargado')
 
-  const btnProbar = document.querySelector('#btnProbar')
+    const btnProbar = document.querySelector('#btnProbar')
 
-  btnProbar.addEventListener('click', onClickProbar)
+    btnProbar.addEventListener('click', onClickProbar)
 
-  document.querySelector('#btnLocalizar').addEventListener('click' , onClick)
+    document.querySelector('#btnLocalizar').addEventListener('click' , onClick)
 
-  
-  async function onClick () {
-      let position = await localizar()
-      console.log(position)
-  }
-
-
-  function onClickProbar() {
-      // window.alert('Probando')
-      // window.prompt('Probando')
-      // window.confirm('Probando')
-
-      let handler = window.open()
-      handler.location.assign('http://google.com')
-      window.setTimeout(() =>{
-          // handler.close()
-      }, 2000)
-      console.log(handler)
-
-      let handTime = setTimeout(() => {}, 2000)
-      clearTimeout(handTime)
-
-      let handInterval = setInterval(() => {}, 10)
-      clearInterval(handInterval)
-  }
+    
+    async function onClick () {
+        let position = await localizar()
+        console.log(position)
+    }
 
 
-  function localizar() {
-      return new Promise( (resolve, reject) => {
-          navigator.geolocation.getCurrentPosition( (position) => {
-              resolve(position)
-          })
-      })
-      
-  }
+    function onClickProbar() {
+        //window.alert('Probando')
+        //window.prompt('Probando')
+        //let x = window.confirm('Probando')
+        //no es necesario poner window. delante, lo reconoce igualmente
+
+        let handler = window.open()
+        handler.location.assign('http://google.com')
+        window.setTimeout(() =>{
+            // handler.close()
+        }, 2000)
+        console.log(handler)
+
+        /*    Funciones de tiempo que ofrece window
+                setInterval()
+                setTimeout() devuelve un handler 
+                clearInterval()
+                clearTimeout() */
+
+        let handTime = setTimeout(() => { }, 2000) //se realiza despues de 2s
+        clearTimeout(handTime) //anula handTime
+
+        let handInterval = setInterval(() => {}, 2000) //cada 2s se repite
+        clearInterval (handInterval) //anula la repeticion
+
+    }
+
+    function localizar() {
+        return new Promise( (resolve, reject) => {
+            navigator.geolocation.getCurrentPosition( (position) => {
+                resolve(position)
+            })
+        })
+        
+    }
+
 }

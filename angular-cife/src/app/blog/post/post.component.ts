35352da6b77cb81a8b5a18bc8d348f42e8321e3a
marenalj <jorquera2009@hotmail.com>
2019-06-26 12:38:34 +0200

    blog

---
+++ angular-cife/src/app/blog/post/post.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: Post;
+  @Input() index: number;
+  @Output() eventBorrar: EventEmitter<any>;
+
+  constructor() {
+    this.eventBorrar = new EventEmitter<number>;
+   }
+
+  ngOnInit() {}
+
+  onClickBorrar() {
+    this.eventBorrar.next(this.index);
+  }
+
+}
